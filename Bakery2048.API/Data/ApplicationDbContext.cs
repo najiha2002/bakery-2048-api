5b88d5e483b56773c224a4f23f81baebca74c0ab
Najiha <najiha2002@gmail.com>
2025-11-29 23:15:00 +0800

    add UTC conversion config

--- Bakery2048.API/Data/ApplicationDbContext.cs
+++ Bakery2048.API/Data/ApplicationDbContext.cs
@@ -22,6 +22,20 @@ public class ApplicationDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
+        // Configure all DateTime properties to use UTC
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime) || property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(new Microsoft.EntityFrameworkCore.Storage.ValueConversion.ValueConverter<DateTime, DateTime>(
+                        v => v.Kind == DateTimeKind.Utc ? v : DateTime.SpecifyKind(v, DateTimeKind.Utc),
+                        v => DateTime.SpecifyKind(v, DateTimeKind.Utc)));
+                }
+            }
+        }
+
         // Player configuration
         modelBuilder.Entity<Player>(entity =>
         {
