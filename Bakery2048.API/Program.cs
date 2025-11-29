8ffd28b660779e871b4c8d5f30a73c56e63ef945
Najiha <najiha2002@gmail.com>
2025-11-29 23:22:12 +0800

    load env vars from .env for postgres

--- Bakery2048.API/Program.cs
+++ Bakery2048.API/Program.cs
@@ -1,9 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Bakery2048.API.Data;
+using DotNetEnv;
+
+// load .env file
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+// build connection string from environment variables
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                      $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                      $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+                      $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 // add controllers
 builder.Services.AddControllers();
