b5717e0bcfdb7215c9a7b54bc83a0be2fed2b102
Najiha <najiha2002@gmail.com>
2025-12-05 21:06:27 +0800

    swagger docu

--- Bakery2048.API/Controllers/TilesController.cs
+++ Bakery2048.API/Controllers/TilesController.cs
@@ -18,8 +18,16 @@ public class TilesController : ControllerBase
     }
 
     // GET: api/tiles - returns all tiles
+    /// <summary>
+    /// Gets all tiles in the game
+    /// </summary>
+    /// <returns>List of all tiles with their properties</returns>
+    /// <response code="200">Returns the list of tiles</response>
+    /// <response code="401">If user is not authenticated</response>
     [Authorize]
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<TileResponseDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<ActionResult<IEnumerable<TileResponseDto>>> GetTiles()
     {
         var tiles = await _tileService.GetAllTiles();
@@ -39,8 +47,19 @@ public class TilesController : ControllerBase
     }
 
     // GET: api/tiles/{id} - returns a specific tile by ID
+    /// <summary>
+    /// Gets a specific tile by ID
+    /// </summary>
+    /// <param name="id">The tile's unique identifier</param>
+    /// <returns>Tile details</returns>
+    /// <response code="200">Returns the tile</response>
+    /// <response code="401">If user is not authenticated</response>
+    /// <response code="404">If tile is not found</response>
     [Authorize]
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(TileResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<TileResponseDto>> GetTile(Guid id)
     {
         var tile = await _tileService.GetTileById(id);
@@ -65,8 +84,21 @@ public class TilesController : ControllerBase
     }
 
     // POST: api/tiles - creates a new tile
+    /// <summary>
+    /// Creates a new tile (Admin only)
+    /// </summary>
+    /// <param name="createTileDto">The tile data to create</param>
+    /// <returns>The newly created tile</returns>
+    /// <response code="201">Returns the newly created tile</response>
+    /// <response code="400">If the tile data is invalid</response>
+    /// <response code="401">If user is not authenticated</response>
+    /// <response code="403">If user is not an Admin</response>
     [Authorize(Roles = "Admin")]
     [HttpPost]
+    [ProducesResponseType(typeof(TileResponseDto), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public async Task<ActionResult<TileResponseDto>> CreateTile(CreateTileDto createTileDto) // receives createTileDto of type CreateTileDto object
     {
         try
@@ -104,8 +136,22 @@ public class TilesController : ControllerBase
     }
 
     // PUT: api/tiles/{id} - updates an existing tile
+    /// <summary>
+    /// Updates an existing tile (Admin only)
+    /// </summary>
+    /// <param name="id">The tile's unique identifier</param>
+    /// <param name="updateTileDto">The updated tile data</param>
+    /// <returns>No content</returns>
+    /// <response code="204">If the tile was successfully updated</response>
+    /// <response code="401">If user is not authenticated</response>
+    /// <response code="403">If user is not an Admin</response>
+    /// <response code="404">If tile is not found</response>
     [Authorize(Roles = "Admin")]
     [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateTile(Guid id, UpdateTileDto updateTileDto)
     {
         // update the tile in the database using the service
@@ -127,8 +173,21 @@ public class TilesController : ControllerBase
     }
 
     // DELETE: api/tiles/{id} - deletes a tile
+    /// <summary>
+    /// Deletes a tile (Admin only)
+    /// </summary>
+    /// <param name="id">The tile's unique identifier</param>
+    /// <returns>No content</returns>
+    /// <response code="204">If the tile was successfully deleted</response>
+    /// <response code="401">If user is not authenticated</response>
+    /// <response code="403">If user is not an Admin</response>
+    /// <response code="404">If tile is not found</response>
     [Authorize(Roles = "Admin")]
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteTile(Guid id)
     {
         var result = await _tileService.DeleteTile(id);
